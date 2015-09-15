91cfb78d78dc8f80aaf173c8694c3cbc79f63ae2
Grant Elliott <gelliott181@gmail.com>
2015-09-14 20:27:08 -0400

    Committing a base for the drawing app.
    Signed-off-by: Grant Elliott <gelliott181@gmail.com>

---
+++ SingNSketch/ViewController.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var mainImageView: UIImageView!
+  @IBOutlet weak var tempImageView: UIImageView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  // Actions
+
+  @IBAction func reset(sender: AnyObject) {
+  }
+
+  @IBAction func share(sender: AnyObject) {
+  }
+  
+  @IBAction func pencilPressed(sender: AnyObject) {
+  }
+}
+
