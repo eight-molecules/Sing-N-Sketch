f4b596f924629bafc2d039b4c69abb2d43ffa18f
Dakota B <dakotab@bgsu.edu>
2015-09-27 17:01:18 -0400

    New Update for the app: Added: Bezier Curves, tool bar that holds the hide, new, and menu (for now), a show button for after you click hide, and the app works universally (iphone/ipad) Fixed: Bug where you draw over the menu while it's up

---
+++ SingNSketch/SketchingView.swift
@@ -0,0 +1,174 @@
+//
+//  SketchingView.swift
+//  Sing N Sketch
+//
+//  Created by Dakota-Cheyenne Brown on 9/27/15.
+//  Copyright (c) 2015 BGSU. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SketchingView: UIView {
+    
+    
+    //stores previously drawn path
+    var drawImage: UIImage!
+    
+    var userBrush: Brush = Brush()
+    
+    
+    @IBOutlet weak var toolBar: UIToolbar!
+    
+    @IBOutlet weak var hide: UIButton!
+    
+    @IBOutlet weak var show: UIButton!
+    
+    @IBAction func hide(sender: UIButton) {
+        toolBar.hidden = true
+        show.hidden = false
+    }
+    
+    @IBAction func show(sender: UIButton) {
+        toolBar.hidden = false
+        show.hidden = true
+    }
+    
+    @IBOutlet weak var newDrawing: UIButton!
+    
+    // New Drawing Action
+    @IBAction func newDrawing(sender: UIButton) {
+        drawImage = nil
+        drawImage?.drawInRect(bounds)
+        setNeedsDisplay()
+        
+    }
+    
+    //stores an array of points for Bezier curves
+    var points = [CGPoint]()
+    
+    var audio: AudioInterface = AudioInterface()
+    
+    // Basic pitch mappings. Do not re-use, we have a framework for this.
+    let noteFrequencies = [16.35,17.32,18.35,19.45,20.6,21.83,23.12,24.5,25.96,27.5,29.14,30.87]
+    let noteNamesWithSharps = ["C", "C♯","D","D♯","E","F","F♯","G","G♯","A","A♯","B"]
+    let noteNamesWithFlats = ["C", "D♭","D","E♭","E","F","G♭","G","A♭","A","B♭","B"]
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .whiteColor()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+    
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        let touch = touches.first as! UITouch
+        points.append(touch.locationInView(self))
+    }
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        let touch = touches.first as! UITouch
+        points.append(touch.locationInView(self))
+        
+        setNeedsDisplay()
+    }
+    
+    override func touchesCancelled(touches: Set<NSObject>!, withEvent event: UIEvent!) {
+        touchesEnded(touches!, withEvent: event)
+    }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        let touch = touches.first as! UITouch
+        points.append(touch.locationInView(self))
+        
+        setNeedsDisplay()
+        
+        UIGraphicsBeginImageContextWithOptions(bounds.size, false, 0)
+        drawViewHierarchyInRect(bounds, afterScreenUpdates: true)
+        drawImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        points.removeAll()
+        
+    }
+    
+    func getMidPoint(a: CGPoint, andB b: CGPoint) -> CGPoint{
+        return CGPoint(x: (a.x + b.x)/2, y: (a.y + b.y)/2)
+    }
+    
+    
+    //Only override drawRect: if you perform custom drawing.
+    //An empty implementation adversly affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Update audio
+        audio.update()
+        
+        let currentContext = UIGraphicsGetCurrentContext()
+        
+        // DEBUG
+        let f = "Frequency: " + audio.frequency.description
+        let a = "Amplitude: " + audio.amplitude.description
+        println(f)
+        println(a)
+        println("")
+        
+        // DEMO CODE - Changes Blue value based on frequency
+        if (audio.amplitude > 0.005) {
+            var frequency: Float = audio.frequency
+            while (frequency > Float(noteFrequencies[noteFrequencies.count-1])) {
+                frequency = frequency / 2.0
+            }
+            while (frequency < Float(noteFrequencies[0])) {
+                frequency = frequency * 2.0
+            }
+            
+            // Set red color
+            let b = CGFloat((frequency - 16) / 16)
+            userBrush.blue = b
+            
+            
+        }
+        
+        //Drawing code
+        //enabling antialiasing
+        CGContextSetAllowsAntialiasing(currentContext, true)
+        CGContextSetShouldAntialias(currentContext, true)
+        
+        let path = UIBezierPath()
+        
+        path.lineCapStyle = kCGLineCapRound
+        path.lineJoinStyle = kCGLineJoinRound
+        path.lineWidth = userBrush.brushWidth
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let components: [CGFloat] = [userBrush.red, userBrush.green, userBrush.blue, userBrush.opacity]
+        let color = CGColorCreate(colorSpace, components)
+        CGContextSetStrokeColorWithColor(currentContext, color)
+        
+        path.removeAllPoints()
+        
+        drawImage?.drawInRect(bounds)
+        
+        //draw a line between first point and mid point, then mid point and last point
+        //quadratic bezier curves are then made
+        if !points.isEmpty {
+            path.moveToPoint(points.first!)
+            path.addLineToPoint(getMidPoint(points.first!, andB: points[1]))
+            
+            for index in 1..<points.count - 1 {
+                let midPoint = getMidPoint(points[index], andB: points[index+1])
+                path.addQuadCurveToPoint(midPoint, controlPoint: points[index])
+            }
+            
+            path.addLineToPoint(points.last!)
+            
+            
+            path.stroke()
+        }
+        
+    }
+}
--- SingNSketch/ViewController.swift
+++ SingNSketch/ViewController.swift
@@ -9,13 +9,14 @@ protocol ViewControllerDelegate {
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var mainImageView: UIImageView!
-    @IBOutlet weak var tempImageView: UIImageView!
     var delegate: ViewControllerDelegate?
     
-    var userBrush: Brush = Brush()
-    var swiped = false
-    var lastPoint = CGPoint.zeroPoint
+    //var userBrush: Brush = Brush()
+    //var swiped = false
+    //var lastPoint = CGPoint.zeroPoint
+    
+    @IBOutlet weak var sketchingView: SketchingView!
+    
     
     var audio: AudioInterface = AudioInterface()
     
@@ -28,124 +29,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sketchingView.frame = view.bounds
+        sketchingView.autoresizingMask = view.autoresizingMask
     }
     
     override func viewDidAppear(animated: Bool) {
-        audio.start()
-        audio.update()
+        sketchingView.audio.start()
+        sketchingView.audio.update()
     }
     
     override func viewWillDisappear(animated: Bool) {
-        audio.stop()
+        sketchingView.audio.stop()
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    // Touches Starting Action
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        swiped = false
-        if let touch = touches.first as? UITouch {
-            lastPoint = touch.locationInView(self.view)
-        }
-    }
-    
-    // Draw Line Helper
-    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
-        // Update audio
-        audio.update()
-
-        // DEBUG
-        let f = "Frequency: " + audio.frequency.description
-        let a = "Amplitude: " + audio.amplitude.description
-        println(f)
-        println(a)
-        println("")
-        
-        // DEMO CODE - Changes Blue value based on frequency
-        if (audio.amplitude > 0.005) {
-            var frequency: Float = audio.frequency
-            while (frequency > Float(noteFrequencies[noteFrequencies.count-1])) {
-                frequency = frequency / 2.0
-            }
-            while (frequency < Float(noteFrequencies[0])) {
-                frequency = frequency * 2.0
-            }
-            
-            // Set red color
-            let b = CGFloat((frequency - 16) / 16)
-            userBrush.blue = b
-        }
-        
-        
-        // Draw into tempImageView to handle the line being drawn
-        UIGraphicsBeginImageContext(view.frame.size)
-        let context = UIGraphicsGetCurrentContext()
-        tempImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
-        
-        // Create the line from the last point
-        // TODO: Bezier curves? Current version
-        //       produces a bunch of straight lines.
-        CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
-        CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
-        
-        // Set line endcap (End shape),
-        // width, color, and blend mode
-        CGContextSetLineCap(context, kCGLineCapRound)
-        CGContextSetLineWidth(context, userBrush.brushWidth)
-        CGContextSetRGBStrokeColor(context, userBrush.red, userBrush.green, userBrush.blue, 1.0)
-        CGContextSetBlendMode(context, kCGBlendModeNormal)
-        
-        // Draw the line
-        CGContextStrokePath(context)
-        
-        // End the drawing context
-        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
-        tempImageView.alpha = userBrush.opacity
-        UIGraphicsEndImageContext()
-    }
-    
-    // Swiped Touch Action
-    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
-        // Call the draw function as the touch
-        // point moves around the view
-        swiped = true
-        if let touch = touches.first as? UITouch {
-            let currentPoint = touch.locationInView(view)
-            drawLineFrom(lastPoint, toPoint: currentPoint)
-        
-            lastPoint = currentPoint
-        }
-    }
-    
-    // Touch Ends Action
-    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
-        if !swiped {
-            drawLineFrom(lastPoint, toPoint: lastPoint)
-        }
-        
-        UIGraphicsBeginImageContext(mainImageView.frame.size)
-        mainImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: kCGBlendModeNormal, alpha: 1.0)
-        tempImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: kCGBlendModeNormal, alpha: userBrush.opacity)
-        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        tempImageView.image = nil
-    }
-    
-    // New Drawing Action
-    @IBAction func newDrawing(sender: UIButton) {
-        self.mainImageView.image = nil
-    }
-    
-    
     
     @IBAction func showMenu(sender: UIButton) {
         self.performSegueWithIdentifier("menuSegue", sender: self)
     }
-
-
-
+    
 }
