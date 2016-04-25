4094e50b905a1a0b616abd1c66da6b0a2db320f6
Grant Elliott <grant@grants-macbook-pro.local>
2016-04-25 03:14:50 -0400

    Menu opens and closes

--- SingNSketch/ViewController.swift
+++ SingNSketch/ViewController.swift
@@ -165,7 +165,6 @@ class ViewController: UIViewController {
     @IBAction func hide(sender: UIButton) {
         show.hidden = false
         self.toolbarView.hidden = true
-        closeMenu()
     }
     
     @IBAction func show(sender: UIButton) {
@@ -183,112 +182,6 @@ class ViewController: UIViewController {
         else {
             debugPrint("Palette Editor already open!")
         }
-    }
-    
-    func drawMenu(sender: UIButton) {
-        if menuView == nil {
-            let menuView = MenuView(frame: CGRect(x: -250, y: 0, width: 250, height: self.view.frame.height))
-            self.view.addSubview(menuView)
-            
-            menuView.open()
-        }
-    }
-    
-    func drawMenu() {
-            
-            // This is bad. All of this is bad, and will be updated to be better.
-            let menuView = MenuView(frame: CGRectMake(-250, 0, 250, self.view.frame.height))
-            menuView.backgroundColor = UIColor.clearColor()
-            menuView.alpha = 1
-            menuView.tag = 100
-            menuView.userInteractionEnabled = true
-            menuView.layer.shadowOffset = CGSize(width: 3, height: -2)
-            menuView.layer.shadowOpacity = 0.7
-            menuView.layer.shadowRadius = 2
-            
-            // Blur Effect
-            let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Dark)
-            let blurEffectView = UIVisualEffectView(effect: blurEffect)
-            blurEffectView.frame = menuView.bounds
-            menuView.addSubview(blurEffectView)
-            
-            if show.hidden == false {
-                let title = UILabel(frame: CGRectMake(10, 0, 230, 40))
-                title.text = navTitle
-                title.backgroundColor = UIColor.clearColor()
-                title.textAlignment = NSTextAlignment.Center
-                title.textColor = UIColor.whiteColor()
-                menuView.addSubview(title)
-                
-            }
-            let offset = (x: CGFloat(0), y: CGFloat(0))
-        
-            let save   = UIButton() as UIButton
-            save.frame = CGRectMake(10, 60, 110, 40)
-            save.backgroundColor = UIColor(white: 0.1, alpha: 0)
-            save.setTitle("Save", forState: UIControlState.Normal)
-            save.addTarget(self, action: "save:", forControlEvents: UIControlEvents.TouchUpInside)
-            save.layer.shadowOffset = CGSize(width: 0, height: 1)
-            save.layer.shadowOpacity = 0.7
-            save.layer.shadowRadius = 2
-            menuView.addSubview(save)
-        
-        
-            let new   = UIButton() as UIButton
-            new.frame = CGRectMake(130, 60, 110, 40)
-            new.backgroundColor = UIColor(white: 0.1, alpha: 0)
-            new.setTitle("New", forState: UIControlState.Normal)
-            new.addTarget(self, action: "new:", forControlEvents: UIControlEvents.TouchUpInside)
-            new.layer.shadowOffset = CGSize(width: 0, height: 1)
-            new.layer.shadowOpacity = 0.7
-            new.layer.shadowRadius = 2
-            menuView.addSubview(new)
-            
-            let undo   = UIButton() as UIButton
-            undo.frame = CGRectMake(10, 110, 110, 40)
-            undo.backgroundColor = UIColor(white: 0.1, alpha: 0)
-            undo.setTitle("Undo", forState: UIControlState.Normal)
-            undo.addTarget(self, action: "undo:", forControlEvents: UIControlEvents.TouchUpInside)
-            undo.layer.shadowOffset = CGSize(width: 0, height: 1)
-            undo.layer.shadowOpacity = 0.7
-            undo.layer.shadowRadius = 2
-            menuView.addSubview(undo)
-            
-            let redo   = UIButton() as UIButton
-            redo.frame = CGRectMake(130, 110, 110, 40)
-            redo.backgroundColor = UIColor(white: 0.1, alpha: 0)
-            redo.setTitle("Redo", forState: UIControlState.Normal)
-            redo.addTarget(self, action: "redo:", forControlEvents: UIControlEvents.TouchUpInside)
-            redo.layer.shadowOffset = CGSize(width: 0, height: 1)
-            redo.layer.shadowOpacity = 0.7
-            redo.layer.shadowRadius = 2
-            menuView.addSubview(redo)
-            
-            let openPaletteEditor   = UIButton() as UIButton
-            openPaletteEditor.frame = CGRectMake(10, 160 + offset.y, 230, 40)
-            openPaletteEditor.backgroundColor = UIColor(white: 0.1, alpha: 0)
-            openPaletteEditor.setTitle("Palette Editor", forState: UIControlState.Normal)
-            openPaletteEditor.addTarget(self, action: "drawPaletteEditor:", forControlEvents: UIControlEvents.TouchUpInside)
-            openPaletteEditor.layer.shadowOffset = CGSize(width: 0, height: 1)
-            openPaletteEditor.layer.shadowOpacity = 0.7
-            openPaletteEditor.layer.shadowRadius = 2
-            menuView.addSubview(openPaletteEditor)
-            
-            let menuSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self,
-                action: "closeMenu")
-            menuSwipeGestureRecognizer.direction = .Left
-            menuView.addGestureRecognizer(menuSwipeGestureRecognizer)
-            
-            self.view.addSubview(menuView)
-            self.view.bringSubviewToFront(toolbarView)
-            
-            UIView.animateWithDuration(0.7, animations: {
-                var menuFrame = menuView.frame
-                menuFrame.origin.x += menuFrame.size.width
-                menuView.frame = menuFrame
-                }
-            )
-        
     }
     
     // Save function for the current canvas
@@ -315,36 +208,18 @@ class ViewController: UIViewController {
     
     
     @IBAction func showMenuView(sender: UIButton) {
-        if (self.view.viewWithTag(100) != nil) {
-            closeMenu()
+        var height: CGFloat = 50
+        if self.toolbarView.frame.height > 50 {
+            height = -50
         }
-        else {
-            drawMenu()
-        }
+        UIView.animateWithDuration(0.7, animations: {
+            var frame = self.toolbarView.frame
+            frame.size.height += height
+            self.toolbarView.frame = frame
+            })
     }
     
-    func swipeMenu(sender: UIScreenEdgePanGestureRecognizer) {
-        if sender.state == .Ended {
-            if self.view.viewWithTag(100) == nil {
-                drawMenu()
-            }
-        }
-    }
     
-    func closeMenu() {
-        if let menuView = self.view.viewWithTag(100) {
-            UIView.animateWithDuration(0.7, animations: {
-                var menuFrame = menuView.frame
-                menuFrame.origin.x -= menuFrame.size.width
-                menuView.frame = menuFrame
-                }, completion: { finished in
-                    menuView.removeFromSuperview()
-                    
-                }
-            )
-        }
-        sketchingView.userInteractionEnabled = true
-    }
     
     @IBAction func new(sender: UIButton) {
         sketchingView.newDrawing()
